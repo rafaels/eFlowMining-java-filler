d75bd1027f2309a68ac674af5c9bfd6485d64437
Rafael Souza <rafael@maisweb.org>
2011-12-17 20:18:37 -0300

    Erro na comparação

--- src/entities/Catch.java
+++ src/entities/Catch.java
@@ -4,7 +4,7 @@ public class Catch extends MethodException {
 	public Catch(Method method, String exception, int startOffSet, int endOffSet) {
 		super(method, exception, "Catch", startOffSet, endOffSet);
 
-		if (exception == "java.lang.Exception" || exception == "java.lang.Error") {
+		if (exception.equals("java.lang.Exception") || exception.equals("java.lang.Error")) {
 			this.setIsGeneric(true);
 		} else {
 			this.setIsGeneric(false);
